5ed8f78d9bc77dcf2406bcd3fcd9981820ff9536
Jamon Holmgren <jamon@clearsightstudio.com>
2014-12-12 20:23:15 -0800

    Create pages if not found

--- lib/thesis/controllers/controller_helpers.rb
+++ lib/thesis/controllers/controller_helpers.rb
@@ -1,15 +1,18 @@
 module Thesis
   module ControllerHelpers
     def current_page
-      slug = request.fullpath.sub(/(\/)+$/,'')
-      @current_page ||= Page.where(slug: slug).first
+      @current_page ||= Page.where(slug: current_slug).first_or_create
       @current_page
+    end
+
+    def current_slug
+      request.fullpath.sub(/(\/)+$/,'')
     end
 
     def root_pages
       @root_pages ||= Page.where(parent_id: nil).order("sort_order ASC")
     end
-    
+
     def thesis_editor
       "<div id='thesis-editor'></div>".html_safe if page_is_editable?(current_page)
     end
